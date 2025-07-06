d4513e97e5f190db5566ab79084f9754e8bf4378
imkunal01 <142741387+imkunal01@users.noreply.github.com>
2025-07-06 21:27:44 +0530

    Created ToDo App

---
+++ src/component/todo.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import './todo.css'
+import Clock from './clock.jsx'
+
+function Todo() {
+    const [inputValue ,setInputval]= useState(" ");
+    const [task , setTask] = useState([]);
+    const handleInputChange = (value) =>{
+        setInputval(value)
+    }
+    const handleFormSubmit = (event) =>{
+        event.preventDefault();
+        if(!inputValue) return;
+        setTask((prev) => [...prev,inputValue] )
+        setInputval("")
+    }
+  return (
+    <section className="todo-container">
+      <header>
+        <h1 className="heading">Todo App</h1>
+      </header>
+      <Clock/>
+      <section className="form">
+        <form onSubmit={handleFormSubmit} >
+            <div>
+                <input type="text" className="todo-input" value={inputValue}onChange={(event) => handleInputChange(event.target.value)}
+                />
+                <button type="submit" className="todo-btn" 
+                >
+                Add Task
+                </button>
+            </div>
+            <div>
+                <ul className="task-list">
+                    {task.map((item, index) => (
+                        <li key={index} className="task-item">{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </form>
+      </section>
+    </section>
+  );
+}
+
+export default Todo;
